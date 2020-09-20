75fdd12c5b98ea55b6e0909038e510b894799cf9
Robertta Costa <roberttaloise@hotmail.com>
2020-09-20 11:59:39 -0300

    starter project

---
+++ src/main/java/com/backyourcompany/BackYourCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.backyourcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackYourCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackYourCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backyourcompany/BackYourCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backyourcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackYourCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
