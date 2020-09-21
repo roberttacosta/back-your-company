add7aa510ca23d4da531ae25ec35f3669e6a5424
Robertta Costa <roberttaloise@hotmail.com>
2020-09-21 14:03:07 -0300

    feat: update and creation of new class for initial database load.

--- src/main/java/com/backyourcompany/BackYourCompanyApplication.java
+++ src/main/java/com/backyourcompany/BackYourCompanyApplication.java
@@ -1,13 +1,58 @@
 package com.backyourcompany;
 
+import com.backyourcompany.database.InitialDatabaseLoad;
+import com.backyourcompany.entities.*;
+import com.backyourcompany.repositories.*;
+import com.backyourcompany.service.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BackYourCompanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackYourCompanyApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(ActivityRepository activityRepository, BillRepository billRepository, CardRepository cardRepository,
+									  HealthInsuranceRepository healthInsuranceRepository, PatientRepository patientRepository, VisitRepository visitRepository,
+									  CardService cardService, ActivityService activityService, HealthInsuranceService healthInsuranceService,
+									  PatientService patientService, VisitService visitService) {
+		return args -> {
+			InitialDatabaseLoad initialDatabaseLoad = new InitialDatabaseLoad();
+
+			List<Activity> activities = activityRepository.findAll();
+			if(activities.isEmpty()){
+				initialDatabaseLoad.addActivity(activityRepository);
+			}
+
+			List<HealthInsurance> healthInsurances = healthInsuranceRepository.findAll();
+			if(healthInsurances.isEmpty()){
+				initialDatabaseLoad.addHealthInsurance(healthInsuranceRepository);
+			}
+
+			List<Patient> patients = patientRepository.findAll();
+			if(patients.isEmpty()){
+				initialDatabaseLoad.addPatient(patientRepository);
+			}
+
+			List<Visit> visits = visitRepository.findAll();
+			if(visits.isEmpty()){
+				initialDatabaseLoad.addVisit(visitRepository);
+			}
+
+			List<Card> cards = cardRepository.findAll();
+			List<Bill> bills = billRepository.findAll();
+			if(cards.isEmpty() && bills.isEmpty()){
+				initialDatabaseLoad.addCardAndBill(cardRepository, activityService, billRepository, healthInsuranceService, patientService, visitService, cardService);
+			}
+
+		};
 	}
 
 }
